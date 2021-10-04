1ba740d9fcce5e7a46eb1c2f4711e26e0cc1c197
Joaquin Morales <joaquin.moralestr@gmail.com>
2021-10-03 22:18:32 -0300

    Se define la estructura base para el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'convertidor',
+    loadChildren: () => import('./pages/convertidor/convertidor.module').then( m => m.ConvertidorPageModule)
+  },
+  {
+    path: 'clima',
+    loadChildren: () => import('./pages/clima/clima.module').then( m => m.ClimaPageModule)
   },
 ];
 
