141c877a23e0c13ee264b6d7a41e49f229f9d555
CristianMRC <cristian.ramirez.canete@gmail.com>
2021-10-13 17:56:45 -0300

    Se agrego codigo convertidor de moneda

--- src/app/pages/convertidor/convertidor.page.ts
+++ src/app/pages/convertidor/convertidor.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MonedasService } from '../../services/convertidor.service';
+import { NavigationExtras } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-convertidor',
@@ -8,8 +11,40 @@ import { Component, OnInit } from '@angular/core';
 export class ConvertidorPage implements OnInit {
 
   pageTitle = 'Convertidor';
+  pageText;
+  acasaPlatita = "";
+  listaMonedas;
+  valorDolar;
+  cantDolares;
+  valorPesos = 0;
 
-  constructor() { }
+  constructor(
+    public monedasServicio: MonedasService,
+    public navController: NavController,) {
+    this.listadoMonedas(); 
+   }
+
+   listadoMonedas() {
+    this.monedasServicio.obtenerMonedas()
+    .then(respuesta => {
+      console.log(respuesta);
+      this.listaMonedas = respuesta;
+      this.valorDolar = respuesta.dolar.valor;
+      this.valorPesos = this.valorDolar * this.cantDolares;
+      if (this.valorPesos > 0)
+          this.pageText = 'Valor en pesos chilenos (CLP) de: ';
+      else 
+          this.pageText = "";
+          this.acasaPlatita = "";     
+      if (this.valorPesos > 20000)
+          this.acasaPlatita= 'A casa platita crack !!! Más de 20 mil CLP en dólares';    
+       
+    },
+    (error) => {
+      console.error(error);
+    }
+   );
+   } 
 
   ngOnInit() {
   }
--- src/app/services/convertidor.service.ts
+++ src/app/services/convertidor.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConvertidorService {
+export class MonedasService {
 
-  constructor() { }
+  urlBase = 'https://mindicador.cl/api'
+
+  constructor(private httpClient: HttpClient) {
+  }
+  
+  obtenerMonedas(): Promise<any> {
+    return new Promise ((resolve, reject) => {
+      this.httpClient.get(this.urlBase)
+      .subscribe(respuesta => {
+        resolve(respuesta);
+      },
+      (err) => {
+        reject(err);
+      });
+    });
+  };
+
 }
